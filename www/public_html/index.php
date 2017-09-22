0b551d6932790b1776c0da2bd56dfe9270399682
AfraidZac <joao.paulo.almeidaaa@gmail.com>
2017-09-22 16:19:19 +0100

    a a a
    changed

--- www/public_html/index.php
+++ www/public_html/index.php
@@ -7,7 +7,7 @@ require_once(TEMPLATES_PATH . "/header.php");
     <div style="background: coral; margin-top: 20px" id="container">
         <div id="content">
             <div style="col-sm-3">
-a a a <b>
+        This is an website for the jjmp brand,  a non-oficialized brand
             </div>
         </div>
         <?php
