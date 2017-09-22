a5f2d963dbf4491c49b8b35add45fad209dc54e2
AfraidZac <joao.paulo.almeidaaa@gmail.com>
2017-09-22 14:53:18 +0100

    First Submit
    First Submit

---
+++ www/resources/templates/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+    "http://www.w3.org/TR/html4/strict.dtd">
+
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>JJMP</title>
+</head>
+
+<body>
+<div id="header"style="margin-left: 30px;width: 100%">
+    <img src="../../public_html/img/logo.png" style="center">
+    <ul class="nav global">
+        <li style="width: 100px;"><a href="#">Home</a></li>
+        <li style="width: 100px;"><a href="#">Articles</a></li>
+        <li style="width: 100px;"><a href="#">Portfolio</a></li>
+    </ul>
+
+</div>
---
+++ www/resources/templates/rightPanel.php
@@ -0,0 +1,8 @@
+<div id="siteControls">
+    <ul class="categories">
+    </ul>
+    <div class="ads">
+        <!-- ads code -->
+    </div>
+
+</div>
