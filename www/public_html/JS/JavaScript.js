845267944f38d75d98f8305cedd1920d7edc8cbe
kymaru <32199434+kymaru@users.noreply.github.com>
2017-09-25 14:29:27 +0100

    heyo

---
+++ www/public_html/JS/JavaScript.js
@@ -0,0 +1,7 @@
+function imgoversizer() {
+    var almeida =  document.getElementById('almeida');
+    
+    almeida.innerHTML="<img onclick='imgoversizer();' src='/public_html/img/j_jjmp.jpg' id='almeida' style='height: 250px; width: 250px;margin-left: 6.250%;margin-right: 6.250%'n' + class='img-thumbnail rounded-circle'>";
+}
+
+
--- www/public_html/aboutus.php
+++ www/public_html/aboutus.php
@@ -3,29 +3,22 @@
 
 ?>
 <html>
-<center>
+
 <div style="height: 100px; margin-bottom: 200px;margin-top: 50px;">
+    <br>
+    <div class="form-group">
+        <center>
+            <img onclick="imgoversizer();" src="/public_html/img/j_jjmp.jpg" id="almeida" style="height: 150px; width: 150px;margin-left: 6.250%;margin-right: 6.250%"
+                 class="img-thumbnail rounded-circle">
+            <img src="/public_html/img/jj_jjmp.jpg" style="height: 150px; width:150px;margin-left: 6.250%;margin-right: 6.250%"
+                 class="img-thumbnail rounded-circle">
+            <img src="/public_html/img/m_jjmp.jpg " style="height: 150px; width: 150px;margin-left: 6.250%;margin-right: 6.250%"
+                 class="img-thumbnail rounded-circle">
+            <img src="/public_html/img/p_jjmp.jpeg" style="height: 150px; width: 150px;margin-left: 6.250%;margin-right: 6.250%"
+                 class="img-thumbnail rounded-circle">
+        </center>
+    </div>
+</div>
 
-    <div>
-        <br>
-        <div class="form-group">
-                    <div class="col-sm-3">
-                        <img src="/public_html/img/j_jjmp.jpg" style="height: 150px; width: 150px;margin-right: 150px" class="img-thumbnail rounded-circle">
-                    </div>
-
-                    <div class="col-sm-3">
-                       <img src="/public_html/img/jj_jjmp.jpg" style="height: 150px; width:150px;margin-right: 150px" class="img-thumbnail rounded-circle">
-                    </div>
-
-                    <div class="col-sm-3">
-                        <img src="/public_html/img/m_jjmp.jpg "style="height: 150px; width: 150px;margin-right: 150px" class="img-thumbnail rounded-circle">
-                    </div>
-
-                    <div class="col-sm-3">
-                        <img src="/public_html/img/p_jjmp.jpeg" style="height: 150px; width: 150px;margin-right: 150px" class="img-thumbnail rounded-circle">
-                    </div>
-        </div>
-
-</center>
 </html>
 
