cdf28e8a1801f4ca296cea3c35b9f39d83ccfdae
AfraidZac <joao.paulo.almeidaaa@gmail.com>
2017-09-25 12:30:13 +0100

    About us

--- www/public_html/aboutus.php
+++ www/public_html/aboutus.php
@@ -3,18 +3,43 @@
 
 ?>
 <html>
-<div style="height: 100px; margin-bottom: 100px">
-    <center>
+<center>
+<div style="height: 100px; margin-bottom: 200px;margin-top: 50px;">
+
     <div>
         <br>
-        <img src="/public_html/img/j_jjmp.jpg" style="height: 150px; width: 150px;margin-right: 100px" class="img-thumbnail rounded-circle">
-        <img src="/public_html/img/jj_jjmp.jpg" style="height: 150px; width:150px;margin-right: 100px" class="img-thumbnail rounded-circle">
-        <img src="/public_html/img/m_jjmp.jpg "style="height: 150px; width: 150px;margin-right: 100px" class="img-thumbnail rounded-circle">
-        <img src="/public_html/img/p_jjmp.jpeg" style="height: 150px; width: 150px;margin-right: 100px" class="img-thumbnail rounded-circle">
+        <div>
+            <table class="center">
+                <tr>
+                    <div class="col-sm-3">
+
+                        <td scope="row"><img src="/public_html/img/j_jjmp.jpg" style="height: 150px; width: 150px;margin-right: 150px" class="img-thumbnail rounded-circle"></td>
+                    </div>
+                    <div class="col-sm-3">
+
+                        <td scope="row"><img src="/public_html/img/jj_jjmp.jpg" style="height: 150px; width:150px;margin-right: 150px" class="img-thumbnail rounded-circle"></td>
+                    </div>
+                    <div class="col-sm-3">
+                        <td scope="row"><img src="/public_html/img/m_jjmp.jpg "style="height: 150px; width: 150px;margin-right: 150px" class="img-thumbnail rounded-circle"></td>
+                    </div>
+                    <div class="col-sm-3">
+                        <td scope="row"><img src="/public_html/img/p_jjmp.jpeg" style="height: 150px; width: 150px;margin-right: 150px" class="img-thumbnail rounded-circle"></td>
+                    </div>
+                </tr>
+                <tr>
+                    <th>&nbsp;&nbsp;&nbsp;&nbsp;João Almeida</th>
+                    <th>&nbsp;&nbsp;&nbsp;&nbsp;João Marques</th>
+                    <th>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Mia Saraiva</th>
+                    <th>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pedro Grilo</th>
+                </tr>
+
+
+            </table>
+        </div>
     </div>
-    </center>
+
 </div>
 
-
+</center>
 </html>
 
