7b5e81a2cc780e63cf50666b961e06040f234c6a
kymaru <32199434+kymaru@users.noreply.github.com>
2017-09-25 10:42:55 +0100

    sobrenos

--- www/public_html/index.php
+++ www/public_html/index.php
@@ -4,8 +4,8 @@ require_once("../resources/config.php");
 
 require_once(TEMPLATES_PATH . "/header.php");
 ?>
-    <div style="background: coral; margin-top: 20px" id="container">
-        <div id="content">
+    <div style="background: ; margin-top: 20px" id="container">
+        <div id="content" style="height: 400px;">
             <div style="col-sm-3">
         This is an website for the jjmp brand, a non-oficialized brand. so yeah this will be deleted.
             </div>
