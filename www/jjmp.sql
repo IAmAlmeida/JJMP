48e8e1a07229405cf188ead3f6ba0dc94f73d28e
kymaru <32199434+kymaru@users.noreply.github.com>
2017-10-03 13:10:49 +0100

    sql

--- www/jjmp.sql
+++ www/jjmp.sql
@@ -8,3 +8,18 @@ pass varchar(255) not null,
 email varchar(255) unique not null,
 primary key(id)
 );
+
+CREATE TABLE if not EXISTS forum (
+idpergunta int auto_increment unique not null ,
+nickname varchar(255) not null,
+pergunta text not null,
+primary key(idpergunta)	
+);
+
+DROP TABLE IF EXISTS respostas;
+CREATE TABLE respostas (
+idpergunta int not null ,
+nickname varchar(255) not null,
+resposta text not null,
+FOREIGN key(idpergunta) REFERENCES forum(idpergunta)
+);
