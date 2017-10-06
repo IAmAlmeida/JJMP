22a16b85ca6aafee0332465046c79badc4d6ee2a
kymaru <32199434+kymaru@users.noreply.github.com>
2017-10-06 15:07:38 +0100

    login5

--- www/resources/templates/header.php
+++ www/resources/templates/header.php
@@ -1,17 +1,7 @@
 <?php
 require_once("../resources/config.php");
 ?>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
-      integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"
-        integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
-        crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js"
-        integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1"
-        crossorigin="anonymous"></script>
+
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
         "http://www.w3.org/TR/html4/strict.dtd">
@@ -54,8 +44,6 @@ require_once("../resources/config.php");
 
         </ul>
         <div class="dropdown">
-            <center>
-
 
         <form class="form-inline my-2 my-lg-0">
 <?php
@@ -67,9 +55,9 @@ if (isset($_POST['login'])) {
 }else{
     echo'
 
- <a class="nav-link active" style="color:#4792ff" data-toggle="modal" data-target="#login">Login</a>
+            <a class="nav-link active" style=" color:#4792ff" data-toggle="modal" data-target="#login">Login</a>
             <a class="nav-link nav-item active" style="color:#4792ff" data-toggle="modal" data-target="#myModal">Registar</a>
-            </center>
+     
 ';
 
 }
@@ -78,7 +66,6 @@ if (isset($_POST['login'])) {
     </div>
 </nav>
 <div id="header" style="margin-left: 30px;width: 100%">
-
 
 </div>
 <?php
