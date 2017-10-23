a5ff820a89dec3707fcf528b9aed7f6668dcf514
AfraidZac <joao.paulo.almeidaaa@gmail.com>
2017-10-23 12:31:34 +0100

    asd

--- www/public_html/JS/ajax.js
+++ www/public_html/JS/ajax.js
@@ -11,3 +11,42 @@ function btnidget(id) {
     xmlhttp.open("POST", "forum.php" );
     xmlhttp.send("id="+ id);
 }
+function validateemail() {
+
+    var request;
+
+    try {
+
+
+        request = new XMLHttpRequest();
+
+    } catch (tryMicrosoft) {
+
+        try {
+
+            request = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (otherMicrosoft) {
+            try {
+                request = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (failed) {
+                request = null;
+            }
+        }
+    }
+
+    var url = "../resources/gateway.php";
+    var emailaddress = document.getElementById("email").value;
+    var vars = "email=" + emailaddress;
+    request.open("POST", url, true);
+
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    request.onreadystatechange = function () {
+        if (request.readyState == 4 && request.status == 200) {
+            var return_data = request.responseText;
+            document.getElementById("validate").innerHTML = return_data;
+        }
+    };
+
+    request.send(vars);
+}
