2c7290d799fb97234f808359404b2e3457bfcdb2
AfraidZac <joao.paulo.almeidaaa@gmail.com>
2017-10-31 12:02:06 +0000

    tomai

--- www/public_html/index.php
+++ www/public_html/index.php
@@ -9,10 +9,10 @@
     ?>
     <head>
         <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="../resources/back.css">
-        <link rel="stylesheet" type="text/css" href="../resources/overlay.css">
-        <link rel="stylesheet" type="text/css" href="../resources/accordion.css">
-        <link rel="stylesheet" type="text/css" href="../resources/dropdown.css">
+        <link rel="stylesheet" type="text/css" href="../resources/css/back.css">
+        <link rel="stylesheet" type="text/css" href="../resources/css/overlay.css">
+        <link rel="stylesheet" type="text/css" href="../resources/css/accordion.css">
+        <link rel="stylesheet" type="text/css" href="../resources/css/dropdown.css">
         <link rel="stylesheet" href="../resources/font-awesome/css/font-awesome.min.css">
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -21,7 +21,7 @@
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="../resources/homeandfooter.css">
+        <link rel="stylesheet" href="../resources/css/homeandfooter.css">
     </head>
 
 
