40d51e35fae509378961e6255ccfde75d7737fe7
AfraidZac <joao.paulo.almeidaaa@gmail.com>
2017-11-10 15:12:58 +0000

    a

--- www/resources/library/formsgateways/semigateway.php
+++ www/resources/library/formsgateways/semigateway.php
@@ -70,10 +70,13 @@ if (isset($_SESSION['email_user']) && $_SESSION['email_user'] != "") {
                     <h4 class=\"modal-title\" style=\"float: right;\">Perfil</h4>
                 </div>
                 <div class=\"modal-body\">
-                
+                <div class='clearfix' style='margin-left: 10px'>
+                   Click for more information
+                </div>
+                <br>
                 <div class='clearfix'>
                 
-                <img name='imguser'  id='imguser' data-toggle='modal' data-target='#imagechanger' src=\"/public_html/img/download.png\" style=\"float: left;height: 100px; width:100px;margin-left: 40px;margin-right: 40px\" class=\"rounded-circle \">
+                <img name='imguser'  id='imguser' data-toggle='modal'  src=\"/public_html/img/download.png\" style=\"float: left;height: 100px; width:100px;margin-left: 40px;margin-right: 40px\" class=\"rounded-circle \">
                 <label id='nick'>Nickname : ".$_SESSION['email_user']."</label><hr style='background-color:#002049; width:78%;'>
                 <label id='nick'>Email : ".$email."</label>
                 
@@ -92,28 +95,6 @@ if (isset($_SESSION['email_user']) && $_SESSION['email_user'] != "") {
         </div>
     </div>
     
-    <div id=\"imagechanger\" class=\"modal fade\" role=\"dialog\">
-        <div class=\"modal-dialog\">
-            <!-- Modal content-->
-            <div class=\"modal-content\">
-
-                <div class=\"modal-header\">
-
-                    <h4 class=\"modal-title\" style=\"float: right;\">ImgChanger</h4>
-                </div>
-                <div class=\"modal-body\">
-                
-                <div class='clearfix'>
-                <img name='imguser' id='imguser' data-toggle='modal' data-target='#imagechanger' src=\"/public_html/img/download.png\" style=\"float: left;height: 100px; width:100px;margin-left: 40px;margin-right: 40px\" class=\"rounded-circle\"> 
-                </div>
-                
-                <button type=\"button\" style=\"background-color: black\" class=\"btn btn-primary btn-block close\"  data-dismiss=\"modal\">&times;</button>
-                
-                
-                </div>
-            </div>
-        </div>
-    </div>
     
     ";
 }
