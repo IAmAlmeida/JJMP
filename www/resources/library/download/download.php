d5fe6ea7dee293c1d693ca37e68fab6dbd85f561
AfraidZac <joao.paulo.almeidaaa@gmail.com>
2017-11-14 23:13:05 +0000

    a
    a

--- www/resources/library/download/download.php
+++ www/resources/library/download/download.php
@@ -1,5 +1,15 @@
 <?php
 ?>
-<div>
+<div style="text-align:;padding: 5%">
 
+    <div style="background-color: #333;color:whitesmoke;padding: 1%">
+        <center>
+            Faça o download da nossa aplicação aqui
+        </center>
+    </div>
+    <div style="background-color: ghostwhite;padding: 3%;">
+        <img src="https://placehold.it/150x80?text=APP IMAGE" style="height: 150px;width: 150px;margin-right: 15px" name="imgdown" id="imgdown">
+        <label style="margin-top: 15px" for="imgdown" class="col-sm-10">Esta é a nossa aplicação, que serve de controlador para o robô.<br> O download será feito apenas por apk por incapacidades financeiras de colocar o mesmo nas plataformas maiores, e funcionará apenas em android. Quando/se a apliação IOS for criada, aparecerá nesta mesma pagina um botão para IOS</label>
+    <button style="margin-top: 5%" class="btn col-sm-12">Brevemente</button>
+    </div>
 </div>
