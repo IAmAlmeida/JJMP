1f8c37968c404d5672f630e1bc26fcc8c1e8c397
AfraidZac <joao.paulo.almeidaaa@gmail.com>
2017-11-15 22:19:00 +0000

    Download

--- www/resources/library/download/download.php
+++ www/resources/library/download/download.php
@@ -4,12 +4,15 @@
 
     <div style="background-color: #333;color:whitesmoke;padding: 1%">
         <center>
-            Faça o download da nossa aplicação aqui
+            <strong>Faça o download da nossa aplicação aqui</strong>
         </center>
     </div>
-    <div style="background-color: ghostwhite;padding: 3%;">
+    <div style="padding: 3%;background-color:#333;">
+        <div style="background-color: ghostwhite;padding: 1%">
         <img src="https://placehold.it/150x80?text=APP IMAGE" style="height: 150px;width: 150px;margin-right: 15px" name="imgdown" id="imgdown">
         <label style="margin-top: 15px" for="imgdown" class="col-sm-10">Nesta página vai ser colocado o link do donwload da nossa aplicação móvel.<br> O link estará brevemente na página quando a aplicação for finalizada.</label>
-    <button style="margin-top: 5%" class="btn col-sm-12">Brevemente</button>
+
+            <button style="margin-top: 5%" class="btn col-sm-12">Brevemente</button>
+        </div>
     </div>
 </div>
