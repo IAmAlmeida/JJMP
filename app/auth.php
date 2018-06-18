ec3264c9662fdb00c08393d4c0a3ebf4e3cb2bc0
PedroGrilo <xpereiragrilo@gmail.com>
2018-06-18 20:07:22 +0100

    12123

--- app/auth.php
+++ app/auth.php
@@ -56,4 +56,22 @@ if(isset($_POST['emailr']))
         echo "error";
 }
 
+if(isset($_POST['userimg'])){
+    $userimg = $_POST["userimg"];
+
+    $sql = "SELECT photo FROM info WHERE nickname ='$userimg';";
+    $result = mysqli_query($con, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while($row = mysqli_fetch_assoc($result)) {
+            $img = $row['photo'];
+            echo $img;
+        }
+    } else {
+        echo "0 results";
+    }
+
+}
+
 ?>
